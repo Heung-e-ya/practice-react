d615dec6546bbfd1d39785bd3b8beb48eaa79164
Heung-e-ya <88697847+Heung-e-ya@users.noreply.github.com>
2023-01-12 14:40:38 +0900

    useEffect, useState+ id추가로 상품경로 찾기

--- src/product/index.js
+++ src/product/index.js
@@ -1,7 +1,23 @@
-import React from "react";
-
-const ProductPage = () => {
-  return <h1>상품 상세페이지</h1>;
-};
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+function ProductPage() {
+  const { id } = useParams();
+  const [product, setProducts] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://d6cb3d34-961d-44d6-baa0-14c40eaffcf5.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProducts(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
+  return <h1>상품 상세 페이지 {id} 상품</h1>;
+}
 
 export default ProductPage;
