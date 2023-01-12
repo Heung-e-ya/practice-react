fcab3e3622bd5cd67232e220c9de86b1db9bdf40
Heung-e-ya <88697847+Heung-e-ya@users.noreply.github.com>
2023-01-12 18:17:25 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,23 +1,45 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./index.css";
+
 function ProductPage() {
   const { id } = useParams();
-  const [product, setProducts] = useState(null);
+  const [product, setProduct] = useState(null);
+
   useEffect(function () {
     axios
       .get(
         `https://d6cb3d34-961d-44d6-baa0-14c40eaffcf5.mock.pstmn.io/products/${id}`
       )
       .then(function (result) {
-        setProducts(result.data);
+        setProduct(result.data);
       })
       .catch(function (error) {
         console.error(error);
       });
   }, []);
-  console.log(product);
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+
+  if (product === null) {
+    return <h1>상품정보를 받고 있습니다..</h1>;
+  }
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <sapn>{product.seller}</sapn>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2023년 01월 12일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
