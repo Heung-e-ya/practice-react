1999e5c86aabfd88b3942c03c346689970f8a44a
Heung-e-ya <88697847+Heung-e-ya@users.noreply.github.com>
2023-01-31 18:39:23 +0900

    resent code-upload,server

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,10 +1,48 @@
-import React from "react";
-import { Form, Divider, Input, InputNumber, Button } from "antd";
+import {
+  Form,
+  Divider,
+  Input,
+  InputNumber,
+  Button,
+  Upload,
+  message,
+} from "antd";
 import "./index.css";
-
-const UploadPage = () => {
+import { ForkOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { API_URL } from "../config/contants";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+function UploadPage() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const history = useHistory();
   const onSubmit = (values) => {
-    console.log(values);
+    axios
+      .post(`${API_URL}/products`, {
+        name: values.name,
+        description: values.description,
+        seller: values.seller,
+        price: parseInt(values.price),
+        imageUrl: imageUrl,
+      })
+      .then((result) => {
+        console.log(result);
+        history.replace("/");
+      })
+      .catch((error) => {
+        console.error(error);
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
   };
   return (
     <div id="upload-container">
@@ -13,10 +51,22 @@ const UploadPage = () => {
           name="upload"
           label={<div className="upload-label">상품 사진</div>}
         >
-          <div id="upload-img-placeholder">
-            <img src="/images/icons/camera.png" />
-            <span>이미지를 업로드해주세요.</span>
-          </div>
+          <Upload
+            name="image"
+            action={`${API_URL}/image`}
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-img" src={`${API_URL}/${imageUrl}`} />
+            ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png" />
+                <span>이미지를 업로드해주세요.</span>
+              </div>
+            )}
+          </Upload>
         </Form.Item>
         <Divider />
         <Form.Item
@@ -54,24 +104,24 @@ const UploadPage = () => {
         <Form.Item
           name="description"
           label={<div className="upload-label">상품 소개</div>}
-          rules={[{ required: true, message: "상품 소개를 입력해주세요" }]}
+          rules={[{ required: true, message: "상품 소개를 입력해주세요." }]}
         >
           <Input.TextArea
+            size="large"
             id="product-description"
             showCount
             maxLength={300}
-            size="large"
-            placeholder="상품 소개를 입력해주세요"
+            placeholder="상품 소개를 적어주세요."
           />
         </Form.Item>
         <Form.Item>
           <Button id="submit-button" size="large" htmlType="submit">
-            문제 등록하기
+            상품 등록하기
           </Button>
         </Form.Item>
       </Form>
     </div>
   );
-};
+}
 
 export default UploadPage;
